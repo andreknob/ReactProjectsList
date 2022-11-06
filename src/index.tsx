d383ff6e7131ee73035792179603d67a409c53f3
André Knob <knobandre@gmail.com>
2022-11-05 21:37:45 -0300

    feat: added initial layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import { store } from "./store";
 import "./index.css";
@@ -9,10 +10,16 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const theme = createTheme({
+  spacing: 4,
+});
+
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </ReduxProvider>
   </React.StrictMode>
 );
--- src/pages/ProjectsList/ProjectsList.tsx
+++ src/pages/ProjectsList/ProjectsList.tsx
@@ -1,10 +1,28 @@
-import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const ProjectsList = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <Typography variant="h1" gutterBottom>
-      ProjectsList
-    </Typography>
+    <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+      <Typography variant="h4" gutterBottom>
+        Projects List
+      </Typography>
+      <TextField
+        label="Project"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+      />
+      <Button>Add project</Button>
+      <Button>Add user</Button>
+    </Box>
   );
 };
 
