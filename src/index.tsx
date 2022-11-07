22fd342ea56b5227c4a5b754d80ba160254bf0e1
André Knob <knobandre@gmail.com>
2022-11-06 21:47:44 -0300

    feat: added dark mode as default

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 
+import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import App from "./App";
 import { store } from "./store";
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -14,12 +16,16 @@ const root = ReactDOM.createRoot(
 
 const theme = createTheme({
   spacing: 4,
+  palette: {
+    mode: "dark",
+  },
 });
 
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </ReduxProvider>
