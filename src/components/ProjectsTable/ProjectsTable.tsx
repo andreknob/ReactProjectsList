81980441f265686e519e204fa49962afcfd03be2
André Knob <knobandre@gmail.com>
2022-11-06 21:41:45 -0300

    feat: added pagination to table

--- src/components/ProjectsTable/ProjectsTable.tsx
+++ src/components/ProjectsTable/ProjectsTable.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import EditIcon from "@mui/icons-material/Edit";
@@ -11,6 +11,8 @@ import {
   TableHead,
   TableRow,
   Paper,
+  TablePagination,
+  Box,
 } from "@mui/material";
 
 import { IProject } from "../../interfaces";
@@ -25,6 +27,8 @@ interface IOwnersMap {
 }
 
 const ProjectsTable = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   const { visibleProjects } = useSelector(selectProjects);
   const { users } = useSelector(selectUsers);
   const dispatch = useDispatch();
@@ -44,7 +48,11 @@ const ProjectsTable = () => {
   };
 
   const renderProjectsRows = () => {
-    return visibleProjects.map((project) => (
+    console.log(page);
+    const sliceInit = page * rowsPerPage;
+    const sliceEnd = sliceInit + rowsPerPage;
+
+    return visibleProjects.slice(sliceInit, sliceEnd).map((project) => (
       <TableRow key={project.id}>
         <TableCell>
           {project.id}{" "}
@@ -61,20 +69,46 @@ const ProjectsTable = () => {
     ));
   };
 
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a projects table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Owner&nbsp;(id)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>{renderProjectsRows()}</TableBody>
-      </Table>
-    </TableContainer>
+    <Box>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 650 }}
+            size="small"
+            aria-label="a projects table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Owner&nbsp;(id)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{renderProjectsRows()}</TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={visibleProjects.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
   );
 };
 
